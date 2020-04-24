6042456b762391fede16786b4ff87ea5099c7d4e
JosueMartinez <jmartinez@cdevteam.com>
2020-04-24 18:11:41 -0400

    adding signalR hub and chat room view

---
+++ JobsityChat/ChatHubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace JobsityChat.ChatHubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            var msgTime = DateTime.Now.ToString("h:mm:ss tt");
+            await Clients.All.SendAsync("ReceiveMessage", username, message, msgTime);
+        }
+    }
+}
---
+++ JobsityChat/wwwroot/js/chat.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message, time) {
+
+    $('<li class="message-container">'
+        + '<p class="font-weight-bold">'+user+':</p>'
+        + '<p>' + message + '</p>'
+        + '<span class="time-right">' + time + '</span>'
+        +'</li>').appendTo('#messagesList');
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("username").value;
+    var message = document.getElementById("message-to-send").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    document.getElementById("message-to-send").value = "";
+    event.preventDefault();
+});
