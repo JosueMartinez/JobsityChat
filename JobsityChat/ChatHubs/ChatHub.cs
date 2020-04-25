8ba58ef62ef3bfbd3ab2446451cf4895ddfaa567
JosueMartinez <jmartinez@cdevteam.com>
2020-04-24 21:15:41 -0400

    bot responding to request from chat

--- JobsityChat/ChatHubs/ChatHub.cs
+++ JobsityChat/ChatHubs/ChatHub.cs
@@ -9,6 +9,19 @@ namespace JobsityChat.ChatHubs
         public async Task SendMessage(string username, string message)
         {
             var msgTime = DateTime.Now.ToString("h:mm:ss tt");
+
+            if (message.Contains("/stock="))
+            {
+                //send original message to all clients js function
+                await Clients.All.SendAsync("ReceiveMessage", username, message, msgTime);
+
+                //get stock message
+                var stockRequest = message.Split('=');
+                var stockCode = stockRequest[1];
+                username = "StockBot";
+                message = StockBot.BotTasks.GetStockMessage(stockCode);
+            }
+            
             //send message to all clients js function
             await Clients.All.SendAsync("ReceiveMessage", username, message, msgTime);
         }
